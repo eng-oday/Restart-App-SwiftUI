d3f4d288d9f7a7fa17790a6dd46de1d8fd46195c
eng-oday <odaydieg@gmail.com>
2023-07-05 09:10:25 +0300

    Initial Commit

---
+++ Restart-App/Restart_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restart_AppApp.swift
+//  Restart-App
+//
+//  Created by 3rabApp-oday on 05/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Restart_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
