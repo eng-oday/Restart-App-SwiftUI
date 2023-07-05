74aa807f997b016978d8a8598803555ebdf69f7d
eng-oday <odaydieg@gmail.com>
2023-07-05 11:20:57 +0300

    first commit

---
+++ Restart-App/Screens/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  Restart-App
+//
+//  Created by 3rabApp-oday on 05/07/2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
---
+++ Restart-App/Screens/OnBoardingView.swift
@@ -0,0 +1,20 @@
+//
+//  OnBoardingView.swift
+//  Restart-App
+//
+//  Created by 3rabApp-oday on 05/07/2023.
+//
+
+import SwiftUI
+
+struct OnBoardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnBoardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnBoardingView()
+    }
+}
