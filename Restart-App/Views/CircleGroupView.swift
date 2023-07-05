965014e434b9ed70be158cebadfc862c2b732c52
eng-oday <odaydieg@gmail.com>
2023-07-05 13:34:38 +0300

    build HomeView

---
+++ Restart-App/Views/CircleGroupView.swift
@@ -0,0 +1,20 @@
+//
+//  CircleGroupView.swift
+//  Restart-App
+//
+//  Created by 3rabApp-oday on 05/07/2023.
+//
+
+import SwiftUI
+
+struct CircleGroupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleGroupView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleGroupView()
+    }
+}
