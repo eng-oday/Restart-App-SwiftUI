dd70a5eedf26cbf1a778f8f8735a9f2068342cdf
eng-oday <odaydieg@gmail.com>
2023-07-05 13:35:48 +0300

    create reusable CircleGroup View

--- Restart-App/Views/CircleGroupView.swift
+++ Restart-App/Views/CircleGroupView.swift
@@ -8,13 +8,32 @@
 import SwiftUI
 
 struct CircleGroupView: View {
+    
+    @State var shapeColor:Color
+    @State var shapeOpacity:CGFloat
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+            ZStack{
+                Circle()
+                    .stroke(shapeColor.opacity(shapeOpacity),lineWidth: 40)
+                    .frame(width: 260,height: 260,alignment: .center)
+                
+                Circle()
+                    .stroke(shapeColor.opacity(shapeOpacity),lineWidth: 80)
+                    .frame(width: 260,height: 260,alignment: .center)
+            }//: ZSTACK
+        
     }
 }
 
 struct CircleGroupView_Previews: PreviewProvider {
     static var previews: some View {
-        CircleGroupView()
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea()
+            
+            CircleGroupView(shapeColor: .white, shapeOpacity: 0.2)
+        }
     }
 }
